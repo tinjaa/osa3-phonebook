4dcf975d42363897c3cc755b28e4c9e299ea4ee6
Sormunen Tinja <tinja.sormunen@solteq.com>
2022-07-12 14:32:18 +0300

    Database configuration

--- index.js
+++ index.js
@@ -26,17 +26,40 @@ app.get('/api/persons', (request, response) => {
   })
 })
 
-app.get('/api/persons/:id',(request,response) => {
-    Person.findById(request.params.id).then(person => {
-        response.json(person)
+app.get('/api/persons/:id',(request,response,next) => {
+    Person.findById(request.params.id)
+      .then(person => {
+        if(person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
     })
+    .catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (request,response) => {
-    const sid = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    
-    response.status(204).end()
+    Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+        response.status(204).end()
+    })
+    .catch(error => next(error))
+})
+
+
+app.put('/api/persons/:id',(request,response,next) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number
+    }
+
+    Person.findByIdAndUpdate(request.params.id,person, {new: true})
+    .then(updatedPerson => {
+        response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons',(request,response) => {
@@ -60,15 +83,32 @@ app.post('/api/persons',(request,response) => {
 })
 
 app.get('/info', (request,response) => {
-    const count = persons.length
     const timeLocation = new Date()
-    console.log(count)
-    console.log(timeLocation)
-
-    response.send(
-        `<p>Phonebook has info for ${count} people</p>
+    
+    Person.find({}).then(people => {
+        response.send(
+        `<p>Phonebook has info for ${people.length} people</p>
         <p>${timeLocation}</p>`)
+    }) 
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+  
+    if (error.name === 'CastError') {
+      return response.status(400).send({ error: 'malformatted id' })
+    }
+  
+    next(error)
+  }
+  
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
