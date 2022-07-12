d45fa402a44754cb75a573abe05d6d2336551412
Sormunen Tinja <tinja.sormunen@solteq.com>
2022-07-12 15:32:48 +0300

    Validation for input field

--- index.js
+++ index.js
@@ -48,21 +48,20 @@ app.delete('/api/persons/:id', (request,response) => {
 
 
 app.put('/api/persons/:id',(request,response,next) => {
-    const body = request.body
+    const { name, number } = request.body
 
-    const person = {
-        name: body.name,
-        number: body.number
-    }
-
-    Person.findByIdAndUpdate(request.params.id,person, {new: true})
+    Person.findByIdAndUpdate(
+        request.params.id
+        ,{ name, number }
+        ,{ new: true, runValidators: true, context: 'query' }
+    )
     .then(updatedPerson => {
         response.json(updatedPerson)
     })
     .catch(error => next(error))
 })
 
-app.post('/api/persons',(request,response) => {
+app.post('/api/persons',(request,response, next) => {
     const body = request.body
 
     if(!body.name|| !body.number) {
@@ -76,9 +75,11 @@ app.post('/api/persons',(request,response) => {
         number: body.number,
     })
 
-    person.save().then(savedPerson => {
+    person.save()
+    .then(savedPerson => {
         response.json(savedPerson)
     })
+    .catch(error => next(error))
 
 })
 
@@ -103,6 +104,8 @@ const errorHandler = (error, request, response, next) => {
   
     if (error.name === 'CastError') {
       return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({error: error.message})
     }
   
     next(error)
--- models/person.js
+++ models/person.js
@@ -13,8 +13,22 @@ mongoose.connect(url)
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: function(v) {
+        return /\d{2,3}-\d/.test(v);
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    minlength: 8,
+    required: true
+  }
 })
 
 personSchema.set('toJSON', {
